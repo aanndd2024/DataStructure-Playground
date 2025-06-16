4f64ef5cac5d5ed30d2d902ba951e147451f6cd9
Anand Yadav <aanand.18jan@outlook.in>
2025-06-16 14:14:46 -0400

    Added one algo

--- MyPlayground.playground/Pages/ReverseString.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/ReverseString.xcplaygroundpage/Contents.swift
@@ -2,6 +2,11 @@
 
 import Foundation
 
+//Input: How are you
+//Output: uoy era woH
+
+let str = "How are you"
+
 func reverseString(string: String) -> String {
     var reverseString: String = ""
     
@@ -11,6 +16,55 @@ func reverseString(string: String) -> String {
     return reverseString
 }
 
-print(reverseString(string: "Hello World"))
+print(reverseString(string: str))
 
+//Input: How are you
+//Output: woH era uoy
+
+func reverseString(str:String) -> String {
+    var resultStr = ""
+    for word in str {
+        resultStr = String(word) + resultStr
+    }
+    
+    return resultStr
+}
+
+let strArr = str.components(separatedBy: .whitespaces)
+
+var resultstring = ""
+for str in strArr {
+    resultstring = resultstring + reverseString(str: str) + " "
+}
+print(resultstring)
+
+//Input: How are you
+//Output: woH era uoy
+
+let str1 = "How are you"
+
+func reverseString1(str:String) -> String {
+    var resultStr = ""
+    var word = ""
+    for char in str {
+        if char.isWhitespace {
+            resultStr = resultStr + reverseWord(str: word) + " "
+            word = ""
+        } else {
+            word = word + String(char)
+        }
+    }
+    resultStr = resultStr + reverseWord(str: word)
+    return resultStr
+}
+
+func reverseWord(str:String) -> String {
+    var resultStr = ""
+    for char in str {
+        resultStr = String(char) + resultStr
+    }
+    return resultStr
+}
+
+print(reverseString(str: str))
 //: [Next](@next)
